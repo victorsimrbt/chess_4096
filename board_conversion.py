099b751ca8360b70dd279c69f58f12a97d0395e2
Sim <v_sim@wincoll.ac.uk>
2021-07-28 11:39:57 +0800

    Second Commit

--- board_conversion.py
+++ board_conversion.py
@@ -15,6 +15,23 @@ chess_dict = {
     '.' : [0,0,0,0,0,0,0,0,0,0,0,0],
 }
 
+value_dict = {
+    'p': [1, 0],
+    'P': [0, 1],
+    'n': [3, 0],
+    'N': [0, 3],
+    'b': [3, 0],
+    'B': [0, 3],
+    'r': [5, 0],
+    'R': [0, 5],
+    'q': [9, 0],
+    'Q': [0, 9],
+    'k': [0, 0],
+    'K': [0, 0],
+    '.': [0, 0]
+}
+
+
 def translate_board(board): 
     pgn = board.epd()
     foo = []  
@@ -31,6 +48,21 @@ def translate_board(board):
         foo.append(foo2)
     return np.array(foo)
 
+def board_matrix(board): 
+    pgn = board.epd()
+    foo = []  
+    pieces = pgn.split(" ", 1)[0]
+    rows = pieces.split("/")
+    for row in rows:
+        foo2 = []  
+        for thing in row:
+            if thing.isdigit():
+                for i in range(0, int(thing)):
+                    foo.append('.')
+            else:
+                foo.append(thing)
+    return np.array(foo)
+
 def translate_move(move):
     from_square = move.from_square
     to_square = move.to_square
@@ -45,3 +77,4 @@ def filter_legal_moves(board,logits):
         filter_mask[from_square,to_square] = 1
     new_logits = logits*filter_mask
     return new_logits
+
---
+++ tactics.py
@@ -0,0 +1,75 @@
+import chess
+from board_conversion import *
+
+def material_counter(board):
+    material = np.array([0,0])
+    translated_board = board_matrix(board)
+    for piece in translated_board:
+        material += value_dict[piece]
+    return material
+
+def pos_cont(board):
+    boards = []
+    legal_moves = list(board.legal_moves)
+    for move in legal_moves:
+        copy_board = board.copy()
+        copy_board.push(move)
+        boards.append(copy_board)
+    return boards,legal_moves
+
+class Node:
+    def __init__(self,board,move,parent):
+        self.board = board
+        self.move = move
+        self.parent_node = parent
+        self.child_nodes = []
+        self.utility = 0
+        self.func = None
+    def evaluate(self,idx):
+        if len(self.child_nodes) == 0:
+            self.utility = material_counter(self.board)
+        else:
+            child_util = [child_node.utility[idx] for child_node in self.child_nodes]
+            self.utility = self.func(child_util)
+    def extend(self):
+        continuations,legal_moves = pos_cont(self.board)
+        for i in range(len(continuations)):
+            self.child_nodes.append(Node(continuations[i],legal_moves[i],self))
+            
+class MinMaxTree():
+    def __init__(self,board):
+        root_node = Node(board,None,None)
+        self.root_node = root_node
+
+    def construct(self,depth = 2):   
+        nodes = []
+        prev_gen = [self.root_node]
+        
+        for i in range(depth):
+            new_gen = []
+            for parent_node in prev_gen:
+                parent_node.extend()
+                new_gen.extend(parent_node.child_nodes)
+            prev_gen = new_gen
+            nodes.append(prev_gen)
+            
+        self.nodes = nodes
+        self.function_list = np.array([[] + [max,min] for _ in range(depth//2)]).flatten()
+        
+
+        return self.root_node
+    
+    def evaluate(self,side):
+        if side == 'White':
+            idx = 0
+        elif side == 'Black':
+            idx = 1
+            
+        for i in range(len(self.nodes)-1,-1,-1):
+            for node in self.nodes[i]:
+                node.func = self.function_list[i]
+                node.evaluate(idx)
+                
+    def predict(self):
+        pred_list = sorted(self.nodes[0], key=lambda x: x.utility)
+        return pred_list[0].move
